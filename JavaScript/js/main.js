eecddc8e35ee5897703af12d9549cb2dd30d7969
Gideone Jr <gideonejr@gmail.com>
2020-12-08 14:46:35 -0300

    Treinando functions

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,1 +1,20 @@
-alert("Relembrando JS");
+function botao(){
+    //alert("Vc apertou o botão");
+    document.getElementById("titulo").innerHTML = "TESTE";
+    
+
+}
+
+function redimencionar(){
+    //window.open("https://www.google.com/");//Outra janela
+
+    window.location.href = "https://www.google.com/"; //Na mesma janela
+}
+
+function trocar(){
+    document.getElementById("mouseMove").innerHTML = "<b>Vc passou o mouse</b>";
+}
+
+function voltar(){
+    document.getElementById("mouseMove").innerHTML = "Passe o mouse";
+}
